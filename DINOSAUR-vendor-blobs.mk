5ce088b5c45bbe4312c3ffc787b4bd382419d8b3
h0sch180 <hoschi@anukis.de>
2019-04-29 21:10:55 +0200

    first commit

---
+++ DINOSAUR-vendor-blobs.mk
@@ -0,0 +1,24 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/etc/.tp,system/etc/.tp)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/etc/firmware,system/etc/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/etc/mddb,system/etc/mddb)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/vendor/lib/,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/vendor/lib/drm,system/vendor/lib/drm)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/vendor/lib/mediadrm,system/vendor/lib/mediadrm)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/CUBOT/DINOSAUR/xbin,system/xbin)
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# android_vendor_cubot_dinosaur
+Android Vendor Blob Cubot DINOSAUR (MT6735A)
